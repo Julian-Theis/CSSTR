dfbcd5c0b135eecf49e73962023435a4e2519bf7
Stephan Fahrenkrog-Petersen <samadeusfp@gmail.com>
2020-03-21 20:26:36 +0100

    Update utils

--- patients/utils.py
+++ patients/utils.py
@@ -1,13 +1,13 @@
 import random
 import string
+from patients.models import Test
 
 def random_string_generator(size=6, chars=string.ascii_uppercase + "123456789"):
     return ''.join(random.choice(chars) for _ in range(size))
 
-def unique_id_generator(instance):
+def unique_id_generator():
     new_id= random_string_generator()
-    Klass= instance.__class__
-    qs_exists= Klass.objects.filter(order_id=new_id).exists()
+    qs_exists= Test.objects.filter(code=new_id).exists()
     if qs_exists:
-        return unique_id_generator(instance)
+        return unique_id_generator()
     return new_id
