84d959f25650c441e414509d5b8f8e7f461174ee
Stephan Fahrenkrog-Petersen <samadeusfp@gmail.com>
2020-03-21 23:30:48 +0100

    Checkpoint -> Save one Test with one patient

--- patients/forms.py
+++ patients/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import Test
+from .models import Patient
 from django.contrib.admin.widgets import AdminDateWidget
+from patients.utils import unique_id_generator
+import datetime
 
 class TestRegistrationForm(forms.ModelForm):
     email = forms.EmailField(required=True)
@@ -17,11 +20,9 @@ class TestRegistrationForm(forms.ModelForm):
     phone = forms.CharField(max_length=100, label="Telefon")
     doctor = forms.CharField(max_length=100, label="Hausarzt")
     insurance = forms.CharField(max_length=100, label="Krankenkasse")
-
-
-    positive_contact = forms.BooleanField(label="Kontakt zu positiver COVID-19 Person?", required=False)
-
-    security = forms.BooleanField(label="Ich bin mit der Datenschutzerklaerung einverstanden")
+    positive_contact = forms.BooleanField(label="Kontakt zu einer mit dem Coronavirus infizierten Person?", required=False)
+    car = forms.BooleanField(label="Kommen Sie mit dem Auto?", required=False)
+    privacy = forms.BooleanField(label="Ich bin mit der Datenschutzerklaerung einverstanden")
 
     class Meta:
         model = Test
@@ -38,5 +39,27 @@ class TestRegistrationForm(forms.ModelForm):
                   'doctor',
                   'insurance',
                   'positive_contact',
-                  'security',
+                  'privacy',
+                  'car'
                   ]
+
+    def save(self,*args, **kwargs):
+        test = self.instance
+        test.email = self.cleaned_data["email"]
+        test.code = unique_id_generator()
+        test.num_people = self.cleaned_data["num_people"]
+        test.car = self.cleaned_data["num_people"]
+        #test.license_plate
+        test.save()
+        Patient.objects.create(
+            test=test,
+            firstname=self.cleaned_data["firstname"],
+            lastname=self.cleaned_data["lastname"],
+            birthdate=self.cleaned_data["birth_date"],
+            zip=self.cleaned_data["zip"],
+            city=self.cleaned_data["city"],
+            phone=self.cleaned_data["phone"],
+            doctor=self.cleaned_data["doctor"],
+            insurance=self.cleaned_data["insurance"],
+            positive_contact=self.cleaned_data["positive_contact"]
+        )
